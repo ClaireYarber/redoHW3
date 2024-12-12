73baa4ffa2e4da2b151b2191f5230ce7d80c4380
ClaireYarber <clyarber@ou.edu>
2024-12-12 00:10:52 -0600

    Create view-header.php

---
+++ view-header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?=$pageTitle?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="f1drivers.php">F1 Drivers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="rank.php">Rank</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="f1driver-with-gp.php">F1 Drivers Grand Prix</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="costofdamage.php">Cost of Damage to Car</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
